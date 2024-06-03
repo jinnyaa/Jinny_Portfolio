57cf710df7a168418511b7b9a578654e124914b3
jinnyaa <oio99661@gmail.com>
2024-06-03 17:57:26 +0900

    240603 업데이트

--- js/index.js
+++ js/index.js
@@ -1,20 +1,43 @@
-$('.menu-txt').on('click', function() {
-    $('.menu-txt').removeClass('active');
-    
-    $(this).addClass('active');
-    
-    var targetClass = '';
-    if ($(this).hasClass('menuhome')) {
-        targetClass = 'home-top';
-    } else if ($(this).hasClass('menuproject')) {
-        targetClass = 'project';
-    } else if ($(this).hasClass('menucarrers')) {
-        targetClass = 'carrers';
-    } else if ($(this).hasClass('menuaboutme')) {
-        targetClass = 'aboutme';
-    }
-    
-    $('html, body').animate({
-        scrollTop: $('.' + targetClass).offset().top
-    }, 800);
-  });
+
+// 메뉴이동
+document.querySelectorAll('.menu-txt').forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        document.querySelectorAll('.menu-txt').forEach(li => {
+            li.classList.remove('active');
+        });
+
+        if (item.classList.contains('menuhome')) {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            const sectionClass = item.classList[1].replace('menu', '');
+            document.querySelector(`section.${sectionClass}`).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+
+        item.classList.add('active');
+    });
+});
+
+window.addEventListener('scroll', () => {
+    document.querySelectorAll('section').forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top >= 0 && rect.top <= window.innerHeight / 2) {
+            document.querySelectorAll('.menu-txt').forEach(li => {
+                li.classList.remove('active');
+            });
+            if (section.classList.contains('home-bottom')) {
+                document.querySelector('.menu-txt.menuhome').classList.add('active');
+            } else {
+                document.querySelector(`.menu-txt.menu${section.className}`).classList.add('active');
+            }
+        }
+    });
+});
+
+
