7214c2828f3351f6c4a5d246bb6dbfd76b596271
jinnyaa <oio99661@gmail.com>
2024-06-04 17:57:03 +0900

    240604 update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+$('#go-to-top').on('click',(e)=>{
+    window.scrollTo({top:0, behavior:"smooth"})
+})
+
 
 // 메뉴이동
 document.querySelectorAll('.menu-txt').forEach(item => {
@@ -40,4 +44,57 @@ window.addEventListener('scroll', () => {
     });
 });
 
+// typing effect
+const typing = document.querySelector('.homebottom-txtbox-typing');
 
+function typingOn(){
+    window.addEventListener('scroll',()=>{
+        if(window.innerHeight > typing.getBoundingClientRect().top){
+            typing.classList.add('on');
+        }
+    })
+}
+
+typingOn();
+
+// mouse-pointer
+document.addEventListener("mousemove", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+  
+    $("#home-bottom-video-cursor").css('transform', 'translate(' + x + 'px, ' + y + 'px)');
+  
+    $('.home-bottom-video').eq(0).on('mouseover', function () {
+      $('.inner_wrap').eq(0).addClass('on');
+    });
+    $('.home-bottom-video').eq(0).on('mouseleave', function () {
+      $('.inner_wrap').eq(0).removeClass('on');
+    });
+  });
+
+  
+
+// project
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        root: null, 
+        rootMargin: '0px',
+        threshold: 0.5 
+    };
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const partClass = entry.target.classList[1];
+                document.querySelector('.project-bg-title.active')?.classList.remove('active');
+                document.querySelector('.project-no').innerText = `( ${partClass[4]} / 3 )`;
+                document.querySelector(`.project-bg-title.${partClass}`).classList.add('active');
+            }
+        });
+    }, observerOptions);
+
+    const projectDetails = document.querySelectorAll('.project-detail');
+    projectDetails.forEach(detail => {
+        observer.observe(detail);
+    });
+});
